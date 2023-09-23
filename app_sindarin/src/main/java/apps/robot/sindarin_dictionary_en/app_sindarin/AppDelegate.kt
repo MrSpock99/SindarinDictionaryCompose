d5b310bd3677745a7870f49b799ddc6af611c283
Ayaz <ayaz.husnutdinov@e-legion.com>
2023-09-23 11:45:43 +0300

    fix baseAppInitializer doubling

--- app_sindarin/src/main/java/apps/robot/sindarin_dictionary_en/app_sindarin/AppDelegate.kt
+++ app_sindarin/src/main/java/apps/robot/sindarin_dictionary_en/app_sindarin/AppDelegate.kt
@@ -29,7 +29,7 @@ class AppDelegate : Application() {
         val initializers = getKoin().getAll<BaseAppInitializer>()
             .sortedByDescending {
                 it.getPriority()
-            }
+            }.distinct()
         initializers.forEach {
             it.onAppStartInit()
         }
--- feature/favorites/favorites_impl/src/main/java/apps/robot/favorites/impl/base/FavoritesBaseModule.kt
+++ feature/favorites/favorites_impl/src/main/java/apps/robot/favorites/impl/base/FavoritesBaseModule.kt
@@ -2,14 +2,15 @@ package apps.robot.favorites.impl.base
 
 import apps.robot.sindarin_dictionary_en.base_ui.presentation.base.BaseAppInitializer
 import apps.robot.sindarin_dictionary_en.base_ui.presentation.base.coroutines.processLifecycleScope
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 fun favoritesBaseModule() = module {
-    factory<BaseAppInitializer> {
+    factory {
         OldToNewFavoritesInitializer(
             coroutineScope = processLifecycleScope,
             oldFavoritesDao = get(),
             favoritesDao = get()
         )
-    }
+    } bind BaseAppInitializer::class
 }
