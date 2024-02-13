41e8c5b58fe2f7b62d89a9c8d732413470c74b5f
Ayaz <ayaz.husnutdinov@e-legion.com>
2024-02-13 10:50:31 +0300

    some refactoring

--- feature/dictionary/dictionary_impl/src/main/java/apps/robot/sindarin_dictionary_en/dictionary/details/DetailsModule.kt
+++ feature/dictionary/dictionary_impl/src/main/java/apps/robot/sindarin_dictionary_en/dictionary/details/DetailsModule.kt
@@ -1,22 +1,10 @@
 package apps.robot.sindarin_dictionary_en.dictionary.details
 
-import apps.robot.sindarin_dictionary_en.dictionary.details.domain.DictionaryGetWordByIdUseCase
-import apps.robot.sindarin_dictionary_en.dictionary.details.domain.DictionaryUpdateWordUseCase
 import apps.robot.sindarin_dictionary_en.dictionary.details.presentation.DetailsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 internal fun detailsModule() = module {
-    factory {
-        DictionaryGetWordByIdUseCase(
-            dictionaryRepository = get(),
-        )
-    }
-    factory {
-        DictionaryUpdateWordUseCase(
-            dictionaryRepository = get(),
-        )
-    }
     viewModel {
         DetailsViewModel(
             setTextToClipboard = get(),
